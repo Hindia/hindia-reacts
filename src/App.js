f9b4fb94256c795ef759f3a2e0fb8ddb38771400
Hindia <hindia.mahamoed@fineto.fi>
2020-02-07 18:05:07 +0200

    checking for automatic build

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This is changed from GitHub</p>
+        <p>automatic build?</p>
         <a
           className="App-link"
           href="https://reactjs.org"
