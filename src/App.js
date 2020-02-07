03d2e220d48ecbf2c4c2b16c370a71e624dbb432
Hindia <hindia.mahamoed@fineto.fi>
2020-02-07 17:46:18 +0200

    changes through GitHub

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>This page does nothing... yet! *wink*</p>
+        <p>This is changed from GitHub</p>
         <a
           className="App-link"
           href="https://reactjs.org"
